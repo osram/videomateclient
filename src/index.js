8624563c658f14e1a14e27d06abbd64109dad83e
unknown <lukasj@kth.se>
2019-03-08 23:32:35 +0100

    belongs to prev commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import { Route} from 'react-router';
 import { BrowserRouter } from 'react-router-dom'
 
-import Manager from './Manager'
+import Manager from './components/Manager'
+import Search from './components/Search'
 import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
     <BrowserRouter>
     <div>
-      <Route path='/' exact component={Manager} />
-      <Route path='/search' component={Manager} />
+      <Route path='/' exact component={Search} />
+      <Route path='/manager' component={Manager} />
     </div>
   </BrowserRouter>,
   document.getElementById('root')
